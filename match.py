6dc87863bf3efcf82d8b5df5a9b4d7d878a6dcf9
Colin Langdon Vernon Cooke <clvcooke@gmail.com>
2017-09-16 13:08:31 -0400

    Create match.py

---
+++ match.py
@@ -0,0 +1,28 @@
+import json
+
+def match_ratio(filename):
+    with open(filename) as f:
+        data = json.load(f)
+#     print(data)
+    target = data['target_allocations']
+    portfolio = data['portfolio_holdings']
+    assets = data['asset_prices']
+    keys = [key for key in target.keys()]
+    values = [portfolio[key]*assets[key] for key in target.keys()]
+#     print(values)
+    total_values = sum(values)
+#     print(total_values)
+    target_values = [total_values*target[key] for key in target.keys()]
+#     print(target_values)
+    # equalize
+    for i in range(len(target_values)):
+        if values[i] < target_values[i]:
+            amount = 0
+            amount = (-values[i] + target_values[i])/assets[keys[i]]
+              
+            print("BOUGHT " + str(amount) + " units of " + keys[i])
+            #but
+        elif values[i] > target_values[i]:
+            # sell
+            amount = (values[i] - target_values[i])/assets[keys[i]]
+            print("SOLD " + str(amount) + " units of " + keys[i])
