084c6b36c44cf1fa20e108462450bc8abc4bff9a
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2017-09-16 13:20:03 -0400

    asd

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ if __name__ == '__main__':
         total_money += data['portfolio_holdings'][name] * data['asset_prices'][name]
 
     for name in data['target_allocations'].keys():
+        current_holdings_percentage = abs(data['asset_prices'][name] * data['portfolio_holdings'][name]) / total_money
+        if abs(current_holdings_percentage - data['target_allocations'][name ]) < 0.005:
+            # don't trade
+            target[name] = data['portfolio_holdings'][name]
+            continue
         curr = 0
         curr_diff = abs(total_money * data['target_allocations'][name] - (curr * data['asset_prices'][name]))
         next_diff = abs(total_money * data['target_allocations'][name] - ((curr + 1) * data['asset_prices'][name]))
@@ -24,12 +29,12 @@ if __name__ == '__main__':
 
     total_trades = 0
     for name in data['target_allocations'].keys():
-        if name != data['portfolio_holdings'][name]:
+        if target[name] != data['portfolio_holdings'][name]:
             total_trades += 1
     print "We placed", total_trades, "trades:"
     for i in range(len(data['target_allocations'].keys())):
         name = data['target_allocations'].keys()[i]
         if target[name] - data['portfolio_holdings'][name] > 0:
             print str(i + 1) + '. Bought', target[name] - data['portfolio_holdings'][name], 'units of', name
-        else:
+        elif target[name] - data['portfolio_holdings'][name] < 0:
             print str(i + 1) + '. Sold', data['portfolio_holdings'][name] - target[name], 'units of', name
